60f7ad9342ceedf1bec319a4743be69e3d9fe918
jigsav6-cmyk <jigsav6@gmail.com>
2026-09-03 22:15:54 +0530

    Create task3_causality_assessment.py

---
+++ task3_causality_assessment.py
@@ -0,0 +1,17 @@
+# ID: task3 - Causality Assessment
+# Intern: Jignesh Savaniya
+
+def assess_causality():
+    print("===== CAUSALITY ASSESSMENT - TASK 3 =====")
+    print("PATIENT: 45-year-old Female")
+    print("DRUG: Amoxicillin")
+    print("REACTION: Skin Rash")
+    print("ONSET: 3 days after starting")
+    print("OUTCOME: Recovered after stopping")
+    print("\n01 TEMPORAL RELATIONSHIP: YES")
+    print("02 DECHALLENGE: POSITIVE")
+    print("03 ALTERNATIVE CAUSES: UNLIKELY")
+    print("\nWHO-UMC: PROBABLE")
+    print("Naranjo Score: 6 = PROBABLE ADR")
+
+assess_causality()
